be0f181068df20b31faade2331f46252287a6757
Qianli Ma <mqianli1@jhu.edu>
2017-05-20 21:52:19 -0400

    removed find_car function from hog_classify.py

--- hog_subsample.py
+++ hog_subsample.py
@@ -24,6 +24,7 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
 
     img_tosearch = img[ystart:ystop,:,:]
     ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    ctrans_tosearch = convert_color(img_tosearch, conv='HSV')
     if scale != 1:
         imshape = ctrans_tosearch.shape
         ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1]/scale), np.int(imshape[0]/scale)))
