e39878e63b7b25c9c716e20acecb0b9bb1df50b4
Qianli Ma <mqianli1@jhu.edu>
2017-05-21 00:59:00 -0400

    testing search_classify.py

--- search_classify.py
+++ search_classify.py
@@ -139,6 +139,9 @@ def find_cars(img, y_start_stop, scale, svc, X_scaler, orient, pix_per_cell, cel
             # Extract the image patch
             subimg = cv2.resize(ctrans_tosearch[ytop:ytop+window, xleft:xleft+window], (64,64))
 
+            # Convert color to LUV for spatial and hist features' best performance
+            subimg = convert_color(subimg, 'LUV')
+
             # Get color features
             spatial_features = bin_spatial(subimg, size=spatial_size)
             hist_features = color_hist(subimg, nbins=hist_bins)
@@ -172,19 +175,23 @@ hist_feat = True
 hog_feat = True
 y_start_stop = [400, 650] # Min and max in y to search in slide_window()
 
-# image = mpimg.imread('bbox-example-image.jpg')
+#image = mpimg.imread('bbox-example-image.jpg')
 image = mpimg.imread('test_images/test1.jpg')
 draw_image = np.copy(image)
+
+# find_car approach
+scale = 1
+out_img = find_cars(image, y_start_stop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, color_space)
+
+plt.imshow(out_img)
+plt.savefig('find-car-bbox-example-image-detected.jpg')
 
 # Uncomment the following line if you extracted training
 # data from .png images (scaled 0 to 1 by mpimg) and the
 # image you are searching is a .jpg (scaled 0 to 255)
 image = image.astype(np.float32)/255
 
-scale = 0.5
-out_img = find_cars(image, y_start_stop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, color_space)
-
-
+# sliding_window approach
 windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
                     xy_window=(96, 96), xy_overlap=(0.5, 0.5))
 
@@ -196,11 +203,6 @@ hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_sp
                         hist_feat=hist_feat, hog_feat=hog_feat)
 
 window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
-
-
-plt.imshow(out_img)
-plt.savefig('find-car-bbox-example-image-detected.jpg')
-
 
 plt.imshow(window_img)
 plt.savefig('search-window-bbox-example-image-detected.jpg')
